59f8c7b496911a276d6294e0e14e8ae198328dd0
Kumkum Gupta <kumkumgupta81699@gmail.com>
2026-02-24 01:08:57 +0530

    Day 1 - Java basics program

---
+++ Day1_Basics.java
@@ -0,0 +1,12 @@
+public class Day1_Basics {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("My name is devdairyy.");
+        System.out.println("I am starting my 30 Days Building Logic Challenge.");
+        System.out.println("I live in Indian.");
+        System.out.println("My goal is to become a strong software developer.");
+
+    }
+}
