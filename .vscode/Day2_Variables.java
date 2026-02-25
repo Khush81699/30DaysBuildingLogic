246c77216a417a42de0195feb81c6c12861860ea
Khush81699 <khushboosgupta864@gmail.com>
2026-02-25 22:31:21 +0530

    Day 2 - Variables added and cleaned project

---
+++ .vscode/Day2_Variables.java
@@ -0,0 +1,18 @@
+public class Day2_Variables {
+
+    public static void main(String[] args) {
+
+        String name = "Devdairyy";
+        int age = 22;
+        double percentage = 85.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My percentage is " + percentage);
+        System.out.println("My grade is " + grade);
+        System.out.println("Am I a student? " + isStudent);
+
+    }
+}
--- Day1_Basics.java
+++ Day1_Basics.java
@@ -1,12 +1,9 @@
 public class Day1_Basics {
-
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
-        System.out.println("My name is devdairyy.");
-        System.out.println("I am starting my 30 Days Building Logic Challenge.");
-        System.out.println("I live in Indian.");
-        System.out.println("My goal is to become a strong software developer.");
-
+        System.out.println("My Name is DevDairyy.");
+        System.out.println("I am starting my 30Days Building Logic Challenge.");
+        System.out.println("I live in India");
+        System.out.println("My goal is to become a strong software developer");
     }
 }
