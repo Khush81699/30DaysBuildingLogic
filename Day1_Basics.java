01cb242772f7fa1c1989467195f9c6f9071feae3
Khushboo Gupta <khushboosgupta864@gmail.com>
2026-02-25 20:25:30 +0530

    Fix typo in location from 'Indian' to 'India'

--- Day1_Basics.java
+++ Day1_Basics.java
@@ -5,7 +5,7 @@ public class Day1_Basics {
         System.out.println("Hello World!");
         System.out.println("My name is devdairyy.");
         System.out.println("I am starting my 30 Days Building Logic Challenge.");
-        System.out.println("I live in Indian.");
+        System.out.println("I live in India.");
         System.out.println("My goal is to become a strong software developer.");
 
     }
