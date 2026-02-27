4abdc61b06eb2dfb1ca44f04ec9ead961b716de5
Khush81699 <khushboosgupta864@gmail.com>
2026-02-27 23:32:54 +0530

    Day 5 - Grade calculator using else if

---
+++ Day5_GradeCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Day5_GradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A+");
+        } 
+        else if (marks >= 75) {
+            System.out.println("Grade: A");
+        } 
+        else if (marks >= 60) {
+            System.out.println("Grade: B");
+        } 
+        else if (marks >= 40) {
+            System.out.println("Grade: C");
+        } 
+        else {
+            System.out.println("Fail");
+        }
+
+        sc.close();
+    }
+}
