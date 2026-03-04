06f0b644b85f1bf2372d6fbb04e19cb49ea585d2
Khush81699 <khushboosgupta864@gmail.com>
2026-03-04 22:33:36 +0530

    Day 9 - Constructor example with default and parameterized constructors

--- Day9_ConstructorExample.java
+++ Day9_ConstructorExample.java
@@ -3,7 +3,14 @@ public class Day9_ConstructorExample {
     String name;
     int age;
 
-    // Constructor
+    // Default Constructor
+    Day9_ConstructorExample() {
+        name = "Unknown";
+        age = 0;
+        System.out.println("Default Constructor Called");
+    }
+
+    // Parameterized Constructor
     Day9_ConstructorExample(String n, int a) {
         name = n;
         age = a;
@@ -13,16 +20,20 @@ public class Day9_ConstructorExample {
     void display() {
         System.out.println("Student Name: " + name);
         System.out.println("Student Age: " + age);
+        System.out.println("--------------------");
     }
 
     public static void main(String[] args) {
 
-        // Creating objects using constructor
-        Day9_ConstructorExample student1 = new Day9_ConstructorExample("Khushboo", 21);
-        Day9_ConstructorExample student2 = new Day9_ConstructorExample("Rahul", 23);
-
+        // Object using default constructor
+        Day9_ConstructorExample student1 = new Day9_ConstructorExample();
         student1.display();
-        System.out.println("-----------");
+
+        // Objects using parameterized constructor
+        Day9_ConstructorExample student2 = new Day9_ConstructorExample("Khushboo", 21);
+        Day9_ConstructorExample student3 = new Day9_ConstructorExample("Rahul", 23);
+
         student2.display();
+        student3.display();
     }
 }
